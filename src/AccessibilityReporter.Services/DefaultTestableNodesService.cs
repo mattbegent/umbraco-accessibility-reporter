7fbfce4e350f45dd67bd9633b0e279163f4e6317
Matt Begent <matt@mattbegent.co.uk>
2025-10-15 17:51:53 +0100

    Fix deprecation warning

--- src/AccessibilityReporter.Services/DefaultTestableNodesService.cs
+++ src/AccessibilityReporter.Services/DefaultTestableNodesService.cs
@@ -1,23 +1,23 @@
 ﻿using AccessibilityReporter.Core.Interfaces;
 using AccessibilityReporter.Services.Interfaces;
 using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Services.Navigation;
 using Umbraco.Cms.Core.Web;
-using Umbraco.Extensions;
 
 namespace AccessibilityReporter.Services
 {
 	public class DefaultTestableNodesService : ITestableNodesService
 	{
 		private readonly IUmbracoContextFactory _contextFactory;
-		private readonly IVariationContextAccessor _variationContextAccessor;
+		private readonly IDocumentNavigationQueryService _documentNavigationQueryService;
 		private readonly IAccessibilityReporterSettings _settings;
 
 		public DefaultTestableNodesService(IUmbracoContextFactory contextFactory,
-            IVariationContextAccessor variationContextAccessor,
+			IDocumentNavigationQueryService documentNavigationQueryService,
 			IAccessibilityReporterSettings settings)
         {
             _contextFactory = contextFactory;
-            _variationContextAccessor = variationContextAccessor;
+            _documentNavigationQueryService = documentNavigationQueryService;
 			_settings = settings;
         }
 
@@ -25,11 +25,19 @@ namespace AccessibilityReporter.Services
 		{
 			using (var contextReference = _contextFactory.EnsureUmbracoContext())
 			{
-				// TODO: *not this*. Perhaps use an examine search. Note - need to think about how we handle cases where ExamineX is used. 
-				// This is currenttly only suitable for small sites.
-				// Aside: Whatever we do here will not be suitable for everyone. We should ensure this implementation can be overriden in userspace. 
-				var everything = contextReference.UmbracoContext.Content!.GetAtRoot()
-					.DescendantsOrSelf<IPublishedContent>(_variationContextAccessor);
+				var rootItems = _documentNavigationQueryService.TryGetRootKeys(out var rootKeys) ? rootKeys : Enumerable.Empty<Guid>();
+
+				var everything = new List<IPublishedContent>();
+
+				foreach (var rootKey in rootItems)
+				{
+					var rootContent = contextReference.UmbracoContext.Content?.GetById(rootKey);
+					if (rootContent != null)
+					{
+						everything.Add(rootContent);
+						everything.AddRange(GetDescendants(rootContent));
+					}
+				}
 
 				return everything.Where(DocumentTypeIsApplicable)
 					.Where(TemplateStateIsApplicable)
@@ -40,6 +48,28 @@ namespace AccessibilityReporter.Services
 
 				bool TemplateStateIsApplicable(IPublishedContent content)
 					=> _settings.IncludeIfNoTemplate || content.TemplateId.HasValue;
+			}
+		}
+
+		private IEnumerable<IPublishedContent> GetDescendants(IPublishedContent content)
+		{
+			if (_documentNavigationQueryService.TryGetChildrenKeys(content.Key, out var childKeys))
+			{
+				foreach (var childKey in childKeys)
+				{
+					// Get the child content from the Umbraco context
+					using var contextReference = _contextFactory.EnsureUmbracoContext();
+					var childContent = contextReference.UmbracoContext.Content?.GetById(childKey);
+					if (childContent != null)
+					{
+						yield return childContent;
+						// Recursively get descendants
+						foreach (var descendant in GetDescendants(childContent))
+						{
+							yield return descendant;
+						}
+					}
+				}
 			}
 		}
 	}
